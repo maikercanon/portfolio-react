94ee2215e7e019bfb3b527023c6f075d0d7de258
maikercanon <maikersito200109@gmail.com>
2025-05-15 16:48:50 -0500

    solve image3

--- src/components/experience/Job.jsx
+++ src/components/experience/Job.jsx
@@ -1,4 +1,5 @@
 import { motion } from "motion/react"
+import certificado from '../../../public/certificado_camara.pdf'
 import '../experience/Job.css'
 
 const Job = ({image, title,tecs, description}) => {
@@ -13,7 +14,7 @@ const Job = ({image, title,tecs, description}) => {
         <h1 className="text-5xl bg-[#112244] p-4 rounded-4xl" >{title}</h1>
         <h1 className="text-7xl  rounded-3xl">{tecs}</h1>
         <p id='p-of-job-content'>{description}</p>
-      <motion.a href="src/assets/certificado_camara.pdf" download
+      <motion.a href={certificado} download
         whileHover={{ scale: 1.05, color: '#007bff' }}
         whileTap={{ scale: 0.95 }}
         transition={{ type: 'spring', stiffness: 300 }}
